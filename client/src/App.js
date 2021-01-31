9679e5b12dcad864c8284c0a6721c2d0535ae825
Masanori Enoki <enoki@adawarp.com>
2021-01-31 23:04:33 +0900

    feat: fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,46 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-class App extends Component {
-  state = {users: []}
+// class App extends Component {
+//   state = {users: []}
 
-  componentDidMount() {
-    fetch('/users')
+//   componentDidMount() {
+//     fetch('/users')
+//       .then(res => res.json())
+//       .then(users => this.setState({ users }));
+//   }
+
+//   render() {
+//     return (
+//       <div classname="App">
+//         <h1>Users</h1>
+//         {this.state.users.map(user =>
+//           <div key="{user.id}">{user.username}</div>
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+
+  const [comment, setComment] = useState('2')
+  useEffect(() => {
+    const loadData = async () => {
+      console.log("hogehoge")
+      await fetch('/users')
       .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
-
-  render() {
-    return (
-      <div classname="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key="{user.id}">{user.username}</div>
-        )}
-      </div>
-    );
-  }
+      .then(users => setComment(users))
+    }
+    console.log('loading...')
+    loadData()
+  }, [])
+  console.log(comment)
+  // // fetch('/users')
+  // //   .then(res => res.json())
+  // //   .then(users => setUsers(users))
+  // console.log(comment)
+  return <h1>Hello! {comment}</h1>
 }
 
 export default App;
